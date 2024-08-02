9d405560ae98904ff43b5fcc4c34ec0cb853a358
adieb <mrizqiamiruladieb@gmail.com>
2024-08-02 21:53:04 +0700

    feat: buat test case login api dan save response ke env (setting api & web integration)

---
+++ Scripts/Login API/Script1722525924600.groovy
@@ -0,0 +1,42 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+import groovy.json.JsonSlurper as JsonSlurper
+
+'Send Request'
+response = WS.sendRequest(findTestObject('kasirAja/API/Authorization - Login'))
+
+//Response Assertion
+WS.verifyResponseStatusCode(response, 201)
+WS.verifyElementPropertyValue(response, 'status', 'success')
+WS.verifyElementPropertyValue(response, 'data.user.email', GlobalVariable.email)
+
+//Parse Response
+def jsonSlurper = new JsonSlurper()
+def jsonResponse = jsonSlurper.parseText(response.getResponseBodyContent())
+
+//Save attribute response value to Global Variable
+GlobalVariable.accessToken = jsonResponse.data.accessToken
+GlobalVariable.refreshToken = jsonResponse.data.refreshToken
+GlobalVariable.id = jsonResponse.data.user.id
+GlobalVariable.email = jsonResponse.data.user.email
+GlobalVariable.name = jsonResponse.data.user.name
+GlobalVariable.role = jsonResponse.data.user.role
+GlobalVariable.officeId = jsonResponse.data.user.officeId
+GlobalVariable.companyId = jsonResponse.data.user.companyId
+GlobalVariable.companyName = jsonResponse.data.user.company_name
